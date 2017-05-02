ac83ee61aaf374b976225a66c9a01ad45280c975
潘星 <panxing@hudongpai.com>
2017-05-02 17:23:09 +0800

    getting started

---
+++ src/main/java/com/panxing/ANoisyCatAppApplication.java
@@ -0,0 +1,12 @@
+package com.panxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ANoisyCatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ANoisyCatAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/panxing/ANoisyCatAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.panxing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ANoisyCatAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
